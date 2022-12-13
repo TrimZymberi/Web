7d463b67e67e87627d9af79cb7fba20c2b50c355
TrimZymberi <tz58110@ubt-uni.net>
2022-12-13 09:42:50 +0100

    js

--- joinus/tipiLlogarie/Biznesi.js
+++ joinus/tipiLlogarie/Biznesi.js
@@ -1,23 +1,38 @@
 function login() {
-        var emailv = document.getElementById("email-label").value;
-		var pass = document.getElementById("password-label").value;
-		var eFilter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-		if(emailv =='') {
-			alert("Shtypni emailin tuaj");
-		}
-		else if(pass =='') {
-        	alert("Shtypni passwordin tuaj");
-		}
-		else if(!eFilter.test(emailv)) {
-			alert("Keni shtypur emailin tuaj gabim ex. yourname@mail.com !");
-		}
-		else if(pass.length < 8 || pass.length > 12) {
-			alert("Passwordi juaj duhet te jete me gjatesi 8 deri 12 karaktere !");
-		}
-		else {
-            alert('Llogaria juaj u hap me sukses');
-	    
-		console.log("prsh");
-		}
+
+	var companyName = document.getElementById('company-name').value;
+	var hqName = document.getElementById('hq-name').value;
+	var nib = document.getElementById('NIB').value;
+	var nfb = document.getElementById('NFB').value;
+	var email = document.getElementById('emaili').value;
+	var password = document.getElementById('passwordi').value;
+  	
+	if (companyName === '') {
+	  alert('Please enter a valid company name.');
+	  return false;
 	}
-	console.log("prsh");
+	if (hqName === '') {
+	  alert('Please enter a valid headquarters name.');
+	  return false;
+	}
+	if (nib === '') {
+	  alert('Please enter a valid NIB.');
+	  return false;
+	}
+	if (nfb === '') {
+	  alert('Please enter a valid NFB.');
+	  return false;
+	}
+	if (email === '') {
+	  alert('Please enter a valid email address.');
+	  return false;
+	}
+	if (password === '' || password.length < 8) {
+	  alert('Please enter a valid password with at least 8 characters.');
+	  return false;
+	}
+  
+
+	return true;
+  }
+  
