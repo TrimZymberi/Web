a9dd27596545491e6ad66571d0f2fbcbc124098c
TrimZymberi <tz58110@ubt-uni.net>
2023-02-20 13:36:50 +0100

    login and logout

---
+++ auth/login.php
@@ -0,0 +1,20 @@
+<?php 
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+if(isset($_POST["submit"])) {
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    include "../packages/database-pkg.php";
+    include "../packages/login-pkg.php";
+    include "../packages/login-controller.php";
+
+    $login = new logControl($username, $password);
+
+    $login->loginUser();
+
+    header("location: /web/Kartell.php?error=none");
+}
