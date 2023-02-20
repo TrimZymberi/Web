30967dc618a5f768b73043c8adab646e02129252
TrimZymberi <tz58110@ubt-uni.net>
2023-02-20 13:38:39 +0100

    register.php

---
+++ auth/register.php
@@ -0,0 +1,21 @@
+<?php 
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+if(isset($_POST["submit"])) {
+
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    include "../packages/database-pkg.php";
+    include "../packages/register-pkg.php";
+    include "../packages/register-controller.php";
+
+    $register = new regControl($username, $email, $password);
+
+    $register->userRegister();
+
+    header("location: /web/Kartell.php?error=none");
+}
