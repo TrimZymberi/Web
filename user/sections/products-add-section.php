5aa288fa8aa227e084415f41cc9aa4c0bb3c7bc7
TrimZymberi <tz58110@ubt-uni.net>
2023-02-21 14:19:40 +0100

    creating products section on dashboard

---
+++ user/sections/products-add-section.php
@@ -0,0 +1,83 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Kartell | Administrator Dashboard</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="../../images/myK.png">
+    <link rel="stylesheet" href="tools-section-style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+    <?php
+    if
+    (isset($_SESSION["userid"])) {
+        if
+        ($_SESSION['usrtype'] == "admin") {
+            ?>
+
+            <div id="whole-box">
+                <div id="main-container">
+                    <div class="side-container">
+                        <h1>Administrator Tools | Products</h1>
+                    </div>
+                    <div class="menu-section">
+                        <form action="../../auth/products-add.php" method="post">
+                            <div class="form-group">
+                                <label for="header">
+                                    <p>Header</p>
+                                </label>
+                                <input type="text" name="header" class="form-control"
+                                    placeholder="Write your Product header here!">
+                            </div>
+                            <div class="form-group">
+                                <label for="price">
+                                    <p>Price</p>
+                                </label>
+                                <input type="text" name="price" class="form-control"
+                                    placeholder="Write your Product price here!">
+                            </div>
+
+                            <div class="form-group image">
+                                <label for="image">
+                                    <p>Image</p>
+                                </label>
+                                <input type="file" name="image" alt="" accept="image/*" value="Upload Image of the Product">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="link">
+                                    <p>Link</p>
+                                </label>
+                                <input type="text" name="link" class="form-control"
+                                    placeholder="Write your link for the chained connection">
+                            </div>
+
+                            <div id="submit">
+                                <button type="submit" name="submit" class="submit-button">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+
+            <?php
+        } else {
+            header("location: /web/Kartell.php?error=usernotfound");
+        }
+    } else {
+        header("location: /web/Kartell.php?error=usernotfound");
+    }
+    ?>
+
+</body>
+
+</html>
---
+++ user/sections/products-section.php
@@ -0,0 +1,59 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Kartell | Administrator Dashboard</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="../images/myK.png">
+    <link rel="stylesheet" href="section-style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+    <?php
+    if (isset($_SESSION["userid"])) {
+        if ($_SESSION['usrtype'] == "admin") {
+            ?>
+
+            <div id="whole-box">
+                <div id="main-container">
+                    <label for="img">
+                        <img src="../../images/dashboard.jpg" alt="images">
+                    </label>
+
+                    <div class="side-container">
+                        <h1>Adminstrator Tools</h1>
+                    </div>
+                    <div class="menu-section">
+
+                        <label for="news"><a href="news-add-section.php">
+                                Products Add Section
+                            </a></label>
+
+                        <label for="news"><a href="news-edit-section.php">
+                                Products Edit Section
+                            </a></label>
+
+                    </div>
+                </div>
+            </div>
+
+
+            <?php
+        } else {
+            header("location: /web/Kartell.php?error=usernotfound");
+        }
+    } else {
+        header("location: /web/Kartell.php?error=usernotfound");
+    }
+    ?>
+
+</body>
+
+</html>
